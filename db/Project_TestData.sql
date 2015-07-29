7ab51a270610f825dcca85a17870841563e1c3e6
Adrian Limon <alimon808@gmail.com>
2015-07-28 17:33:03 -0700

    add project and user sql scripts

---
+++ db/Project_TestData.sql
@@ -0,0 +1,22 @@
+
+-- VERIFY THE SYNTAX!!! SCRIPT HAS BEEN CREATED AUTOMATICALLY WITH THE BEST GUESS!!! ---    
+-- Project --------------
+
+Insert into [Project]
+   ([ProjectName]  , [Active]  , [POP3Address] )
+Values 
+   (  'Alpha'   ,   1  ,   'alpha@example.com'  )
+GO
+
+
+Insert into [Project]
+   ([ProjectName]  , [Active]  , [POP3Address] )
+Values 
+   (  'Beta'   ,   0  ,   'beta@example.com'  )
+GO
+
+Insert into [Project]
+   ([ProjectName]  , [Active]  , [POP3Address] )
+Values 
+   (  'Skunkworks'   ,   0  ,   'skunkworks@example.com'  )
+GO
